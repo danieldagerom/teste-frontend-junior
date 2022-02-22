9a2475b0da85406b8c3062e61281650288550c8a
danieldagerom <danielprimo9@gmail.com>
2022-02-21 23:39:05 -0300

    Implementa classe playlist

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,3 +1,26 @@
+import { Album } from './Album';
+
 export class Playlist implements PlaylistType {
-  // Code here...
+  private _albums: AlbumType[];
+
+  constructor() {
+    this._albums = [];
+  }
+
+  addAlbum(data: AlbumData) {
+    let album: AlbumType = new Album(data);  
+    this._albums.push(album);
+  }
+
+  isFirstAlbum(index: number): boolean {
+    return index === 0;
+  }
+
+  isLastAlbum(index: number): boolean {
+    return index === this._albums.length - 1;
+  }
+
+  public get albums(): AlbumType[] {
+    return this._albums;
+  }
 }
